6fc5da6ae320bd951d76d3d2c5bf43e3e0e1a34c
vishak vijayan <vishakvijayan2002@gmail.com>
2026-06-25 14:32:24 +0530

    Setup DataBase and Table Creation

---
+++ BulkyWebSite/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
