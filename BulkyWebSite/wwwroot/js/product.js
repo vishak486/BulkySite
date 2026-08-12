ef7b2c869175d2e41a85fdee604f46378e0494b2
vishak vijayan <vishakvijayan2002@gmail.com>
2026-08-12 14:36:10 +0530

    Setup and Fixed Ajax code and setup a Sweetalert also

--- BulkyWebSite/wwwroot/js/product.js
+++ BulkyWebSite/wwwroot/js/product.js
@@ -1,20 +1,61 @@
-﻿$(document).ready(function () {
+﻿var dataTable;
+
+$(document).ready(function () {
     loadDataTable();
 });
 
-function loadDataTable
+function loadDataTable()
 {
-    dataTable = new DataTable('#tblData', {
-        "ajax": '/admin/product/getall'
-    },
-        "columns": [
-        { data: 0 },
-        { data: 1 },
-        { data: 2 },
-        { data: 3 },
-        { data: 4 },
-        { data: 5 }
-    ]
-   );
+    dataTable = $('#tblData').DataTable({
+        ajax: {
+            url: '/admin/product/getall',
+            dataSrc: 'data'
+        },
+        columns: [
+            { data: "title", width: "15%" },
+            { data: "isbn", width: "10%" },
+            { data: "listPrice", width: "5%" },
+            { data: "author", width: "15%" },
+            { data: "category.name", width: "15%" },
+            {
+                data: "id",
+                render: function (data) {
+                    return `
+                    <div class="w-50 btn-group" role="group">
+                        <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i> Edit
+                        </a>
+                        <a onClick=Delete("/admin/product/delete/${data}") class="btn btn-danger mx-2">
+                            <i class="bi bi-trash-fill"></i> Delete
+                        </a>
+                    </div>`;
+                },
+                width: "20%"
+            }
+        ]
+    });
 }
 
+function Delete(url)
+{
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message)
+                }
+            })
+        }
+    });
+}
